a9dcf7987b72539cec7f1a69ffd32a58632d040f
pamat <zlamstn91@gmail.com>
2026-01-19 21:48:03 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold underline">
+        Hello, Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
